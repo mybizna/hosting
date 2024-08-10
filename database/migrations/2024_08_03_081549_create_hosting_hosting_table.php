7e28f302216b8cfd0177ab326a0c600c737cbbfb
Dedan <dedanirungu@gmail.com>
2024-08-10 12:04:33 +0300

    Changes to files: database/migrations/2024_08_03_081549_create_hosting_hosting_table.php

--- database/migrations/2024_08_03_081549_create_hosting_hosting_table.php
+++ database/migrations/2024_08_03_081549_create_hosting_hosting_table.php
@@ -34,11 +34,11 @@ return new class extends Migration
             $table->boolean('is_synced')->nullable()->default(false);
             $table->integer('call_counter')->nullable();
             $table->boolean('has_error')->nullable()->default(false);
-            $table->foreignId('domain_id')->nullable();
-            $table->foreignId('package_id')->nullable();
-            $table->foreignId('payment_id')->nullable();
-            $table->foreignId('server_id')->nullable();
-            $table->integer('partner_id')->nullable();
+            $table->foreignId('domain_id')->constrained('hosting_domain')->onDelete('cascade')->nullable()->index('domain_id');
+            $table->foreignId('package_id')->constrained('hosting_packagae')->onDelete('cascade')->nullable()->index('package_id');
+            $table->foreignId('payment_id')->constrained('account_payment')->onDelete('cascade')->nullable()->index('payment_id');
+            $table->foreignId('server_id')->constrained('hosting_server')->onDelete('cascade')->nullable()->index('server_id');
+            $table->foreignId('partner_id')->constrained('partner_partner')->onDelete('cascade')->nullable()->index('partner_id');
             $table->integer('whmcs_order_id')->nullable();
             $table->boolean('is_in_whmcs')->nullable()->default(false);
 
