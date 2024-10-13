09554c4b223fea5972b5228c5978b487424a6137
Dedan <dedanirungu@gmail.com>
2024-10-13 16:29:19 +0300

    Release 2024.12

---
+++ app/Filament/Resources/HostingResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Hosting\Filament\Resources\HostingResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
---
+++ app/Filament/Resources/HostingResource/Pages/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Hosting\Filament\Resources\HostingResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\ListingBase;
+
+// Class List that extends ListBase
+class Listing extends ListingBase
+{
+}
